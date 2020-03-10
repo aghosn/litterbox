4321d2ea4aa1d0da1ae8b979a499cd9c1f67bbb3
Adrien Ghosn <ghosn.adrien@gmail.com>
2020-03-10 13:00:21 +0100

    Trying to restructure

--- types.go
+++ types.go
@@ -2,18 +2,25 @@ package litterbox
 
 import ()
 
+type Domain struct {
+	config *SandboxDomain
+	SView  map[*Package]uint8
+	SPkgs  []*Package
+}
+
 type SandboxDomain struct {
 	Id   string
 	Func string
 	Sys  SyscallMask
-	View map[*Package]uint8
-	Pkgs []*Package
+	View map[string]uint8
+	Pkgs []string
 }
 
 type Package struct {
-	Name  string
-	Id    int
-	Sects []Section
+	Name    string
+	Id      int
+	Sects   []Section
+	Dynamic []Section
 }
 
 type Section struct {
